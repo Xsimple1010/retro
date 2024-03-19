210e55171c5a7b910475ca9e3b3dec72ea82f66a
xsimple1010 <aderval758@gmail.com>
2024-03-19 06:20:25 -0300

    init

---
+++ android/app/src/main/kotlin/com/example/retro/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.retro
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ native/hub/src/lib.rs
@@ -0,0 +1,24 @@
+//! This `hub` crate is the
+//! entry point of the Rust logic.
+
+// This `tokio` will be used by Rinf.
+// You can replace it with the original `tokio`
+// if you're not targeting the web.
+use tokio_with_wasm::tokio;
+
+mod messages;
+// mod sample_functions;
+
+rinf::write_interface!();
+
+// Always use non-blocking async functions
+// such as `tokio::fs::File::open`.
+// If you really need to use blocking code,
+// use `tokio::task::spawn_blocking`.
+async fn main() {
+    // Repeat `tokio::spawn` anywhere in your code
+    // if more concurrent tasks are needed.
+    // tokio::spawn(sample_functions::tell_numbers());
+    // tokio::spawn(sample_functions::stream_fractal());
+    // tokio::spawn(sample_functions::run_debug_tests());
+}
