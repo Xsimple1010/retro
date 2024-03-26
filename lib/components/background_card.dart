b8e8bb1a31528f13db9ac68f8269c1b6bcad8a67
xsimple1010 <aderval758@gmail.com>
2024-03-25 22:34:14 -0300

    add onTab and border radius

--- lib/components/background_card.dart
+++ lib/components/background_card.dart
@@ -9,51 +9,51 @@ class BgCard extends StatelessWidget {
     required this.data,
     required this.width,
     required this.height,
+    required this.onTab,
   });
 
   final GameData data;
   final double width;
   final double height;
+  final Function onTab;
 
   @override
   Widget build(BuildContext context) {
     const borderRadius = BorderRadius.all(
       Radius.circular(12),
     );
-    return Container(
+    return SizedBox(
       width: width,
       height: height,
-      decoration: BoxDecoration(
-        borderRadius: const BorderRadius.all(
-          Radius.circular(12),
-        ),
-        border: Border.all(
-          width: 1.4,
-          color: Theme.of(context).colorScheme.primary,
-        ),
-      ),
-      child: Visibility(
-        visible: data.bg?.isNotEmpty ?? false,
-        replacement: Container(
-          decoration: const BoxDecoration(
+      child: InkWell(
+        onTap: () => onTab(),
+        borderRadius: borderRadius,
+        child: Visibility(
+          visible: data.bg?.isNotEmpty ?? false,
+          replacement: Container(
+            decoration: const BoxDecoration(
+              borderRadius: borderRadius,
+              gradient: LinearGradient(
+                colors: [
+                  Color.fromARGB(255, 22, 146, 155),
+                  Color.fromARGB(255, 67, 32, 197),
+                ],
+              ),
+            ),
+            child: Center(
+              child: Text(
+                data.name.characters.first,
+              ),
+            ),
+          ),
+          child: ClipRRect(
             borderRadius: borderRadius,
-            gradient: LinearGradient(
-              colors: [
-                Color.fromARGB(255, 22, 146, 155),
-                Color.fromARGB(255, 67, 32, 197),
-              ],
+            child: Image.file(
+              File(data.img ?? ""),
+              fit: BoxFit.cover,
+              width: width,
             ),
           ),
-          child: Center(
-            child: Text(
-              data.name.characters.first,
-            ),
-          ),
-        ),
-        child: Image.file(
-          File(data.img ?? ""),
-          fit: BoxFit.cover,
-          width: width,
         ),
       ),
     );
